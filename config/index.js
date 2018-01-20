6da13979452c6c5b69f9bb1e3d32bffd5c12917a
jack <2959921118@qq.com>
2018-01-21 00:17:34 +0800

    build

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/question-answer/dist/',
+    assetsPublicPath: '/vue-mobile-question/dist/',
 
     /**
      * Source Maps
