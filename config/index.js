ff47b1bef045b53b8f6bdbeba4a2a098a65e3487
jack <2959921118@qq.com>
2018-01-20 23:52:09 +0800

    build

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/question-answer/dist',
+    assetsPublicPath: '/question-answer/dist/',
 
     /**
      * Source Maps
